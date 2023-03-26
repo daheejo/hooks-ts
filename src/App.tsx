8c12d800634490a2a53a6e9b5dc4e64e21df88d8
daheejo <happyj6528@gmail.com>
2023-03-26 21:37:30 +0900

    usecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Counter from "./Counter"
-
+import { CounterProvider } from "./context/CounterContext"
+import { initState } from "./context/CounterContext"
 function App() {
 
   return (
     <div className="App">
+      <CounterProvider count={initState.count} text={initState.text}>
      <Counter>{(num: number)=> <>Current count: {num}</>}</Counter>
+     </CounterProvider>
      </div>
   )
 }
